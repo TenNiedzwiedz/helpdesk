da54f48bae1246b9c295ec1d14219c2763ad9b5c
Wojciech Krawczyk <krawczyk_wojciech@wp.pl>
2023-03-17 12:56:14 +0100

    add create and index users views

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts/dashboard')
+
+@section('content')
+
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Dodaj nowego użytkownika</h6>
+        </div>
+        <div class="card-body">
+            <form method="POST" action="/users" class="form-group">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Imię i nazwisko</label>
+                    <select name="role"
+                        class="form-control form-select @error('name') is-invalid @enderror">
+                            <option hidden>Wybierz typ użytkownika</option>
+                        @foreach($roles as $key => $role)
+                            <option value="{{ $key }}">{{ $role }}</option>
+                        @endforeach
+                    </select>
+                    
+                    @error('name')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="name">Imię i nazwisko</label>
+                    <input name="name"
+                        type="text"
+                        value="{{ old('name') }}"
+                        class="form-control form-control-user @error('name') is-invalid @enderror">
+                    
+                    @error('name')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input name="email"
+                        type="email"
+                        value="{{ old('email') }}"
+                        class="form-control form-control-user @error('email') is-invalid @enderror">
+                    
+                    @error('email')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Hasło</label>
+                    <input name="password"
+                        type="password"
+                        class="form-control form-control-user @error('password') is-invalid @enderror">
+                    
+                    @error('password')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="form-group">
+                    <label for="password_confirmation">Powtórz hasło</label>
+                    <input name="password_confirmation"
+                        type="password"
+                        class="form-control form-control-user">
+                </div>
+
+                <div class="d-flex flex-row-reverse">
+                    <button type="submit" class="btn btn-primary btn-icon-split">
+                        <span class="icon text-white-50">
+                            <i class="fas fa-flag"></i>
+                        </span>
+                        <span class="text">Dodaj użytkownika</span>
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts/dashboard')
+
+@section('content')
+<h1 class="h3 mb-4 text-gray-800">Użytkownicy</h1>
+
+<div class="card shadow mb-4">
+    <div class="card-header bg-primary py-3">
+        <h6 class="m-0 font-weight-bold text-gray-100">Lista użytkowników</h6>
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Imię i nazwisko</th>
+                <th scope="col">Email</th>
+                <th scope="col">Typ konta</th>
+                <th scope="col">Data dodania</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                <tr>
+                    <th scope="row">{{ $loop->iteration }}</th>
+                    <td><a href="{{ url('/users')}}/{{ $user->id}}">{{ $user->name }}</a></td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->role() }}</td>
+                    <td>{{ $user->created_at->format('Y-m-d') }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
