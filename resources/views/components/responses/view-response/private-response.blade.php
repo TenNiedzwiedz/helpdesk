63e86980b70d90ee4f33f2d0fe873f15b9eb1635
Wojciech Krawczyk <krawczyk_wojciech@wp.pl>
2023-04-01 01:07:22 +0200

    add private response type

---
+++ resources/views/components/responses/view-response/private-response.blade.php
@@ -0,0 +1,11 @@
+<div class="card shadow mb-4 border-left-info">
+    <div class="d-flex justify-content-between align-items-center card-header bg-gray-200 py-3">
+        <h6 class="m-0">Użytkownik <strong>{{ $response->author->name }}</strong> dodał notatkę:</h6>
+        <div>
+            <small class="mr-2">{{ $response->created_at->format("Y-m-d H:m:s") }}</small>
+        </div>
+    </div>
+    <div class="card-body">
+        {{ $response->content }}
+    </div>
+</div>
