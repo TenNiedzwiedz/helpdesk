281bc0ed9a8ee94fee80c2eb48a500bb649831d9
Wojciech Krawczyk <krawczyk_wojciech@wp.pl>
2023-04-01 02:24:16 +0200

    add private response type

---
+++ resources/views/components/responses/add-response/private-response.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <form method="POST" action="/response/1/{{ $submission->id }}" class="form-group">
+        @csrf
+        <div class="form-group">
+            <label for="content">Treść</label>
+            <textarea name="content"
+                type="text"
+                class="form-control form-control-user @error('title') is-invalid @enderror">{{ old('content') }}</textarea>
+            
+            @error('content')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
+        <div class="d-flex flex-row-reverse">
+            <button type="submit" class="btn btn-info btn-icon-split">
+                <span class="icon text-white-50">
+                    <i class="fas fa-flag"></i>
+                </span>
+                <span class="text">Dodaj notatkę</span>
+            </a>
+        </div>
+    </form>
+</div>
