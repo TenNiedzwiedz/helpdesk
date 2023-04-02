db86e3702b16d32b2439aff98b3f025df417db00
Wojciech Krawczyk <krawczyk_wojciech@wp.pl>
2023-04-02 15:32:45 +0200

    add auth middleware

--- routes/web.php
+++ routes/web.php
@@ -17,27 +17,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::middleware(['auth'])->get('/', function () {
     return view('main');
 });
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/auth', [AuthController::class, 'authenticate']);
-Route::get('/logout', [AuthController::class, 'logout']);
+Route::middleware(['auth'])->get('/logout', [AuthController::class, 'logout']);
 
-Route::controller(UsersController::class)->group(function () {
+Route::middleware(['auth'])->controller(UsersController::class)->group(function () {
     Route::get('users', 'viewAll');
     Route::get('users/new', 'create');
     Route::post('users', 'store');
 });
 
-Route::controller(SubmissionsController::class)->group(function () {
+Route::middleware(['auth'])->controller(SubmissionsController::class)->group(function () {
     Route::get('submissions', 'viewAll');
     Route::get('submissions/new', 'create');
     Route::post('submissions', 'store');
     Route::get('submissions/{id}', 'view');
 });
 
-Route::controller(ResponsesController::class)->group(function () {
+Route::middleware(['auth'])->controller(ResponsesController::class)->group(function () {
     Route::post('response/{type}/{submission_id}', 'store');
 });
